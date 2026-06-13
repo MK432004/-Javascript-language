c3d98aa09d073cefdc9bd4b5e6b351d352dedd43
Manishkumar <manish089808@gmail.com>
2026-06-13 15:19:41 +0530

    html element

---
+++ Basic5/reduse.js
@@ -0,0 +1,12 @@
+const mayarry = [ 1, 2, 3 ,4,]
+// const newnums = mayarry.reduce( function (acc , currnvalu) {
+//     console.log(` acc ${acc} and currnvlaue ${currnvalu}`)
+//     return acc+currnvalu
+
+// }, 0)
+
+// reduse in arrowfunction
+// const newnums = mayarry.reduce((acc , curntvalues) => acc+curntvalues , 0)
+// console.log(newnums)
+
+  
